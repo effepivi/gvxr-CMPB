3f6cc5df5aee003c88c782bf605b6fa620713e8f
Dr Franck P. Vidal <franck.p.vidal@fpvidal.net>
2021-11-10 12:31:25 +0000

    Add code to convert a SITK image into a VTK image.

---
+++ notebooks/sitk2vtk.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+
+"""
+Function to convert a SimpleITK image to a VTK image.
+
+Written by David T. Chen from the National Institute of Allergy
+and Infectious Diseases, dchen@mail.nih.gov.
+It is covered by the Apache License, Version 2.0:
+http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+import SimpleITK as sitk
+import vtk
+from vtk.util import numpy_support
+
+
+def sitk2vtk(img, debugOn=False, centre=False):
+    """Convert a SimpleITK image to a VTK image, via numpy."""
+
+    size = list(img.GetSize())
+    spacing = list(img.GetSpacing())
+    ncomp = img.GetNumberOfComponentsPerPixel()
+    direction = img.GetDirection()
+    origin = list(img.GetOrigin())
+    
+    if centre:
+        origin[0] = -spacing[0] * size[0] / 2 + spacing[0] / 2
+        origin[1] = -spacing[1] * size[1] / 2 + spacing[1] / 2
+        origin[2] = -spacing[2] * size[2] / 2 + spacing[2] / 2
+
+    # there doesn't seem to be a way to specify the image orientation in VTK
+
+    # convert the SimpleITK image to a numpy array
+    i2 = sitk.GetArrayFromImage(img)
+    if debugOn:
+        i2_string = i2.tostring()
+        print("data string address inside sitk2vtk", hex(id(i2_string)))
+
+    vtk_image = vtk.vtkImageData()
+
+    # VTK expects 3-dimensional parameters
+    if len(size) == 2:
+        size.append(1)
+
+    if len(origin) == 2:
+        origin.append(0.0)
+
+    if len(spacing) == 2:
+        spacing.append(spacing[0])
+
+    if len(direction) == 4:
+        direction = [ direction[0], direction[1], 0.0,
+                      direction[2], direction[3], 0.0,
+                               0.0,          0.0, 1.0 ]
+
+
+    vtk_image.SetDimensions(size)
+    vtk_image.SetSpacing(spacing)
+    vtk_image.SetOrigin(origin)
+    vtk_image.SetExtent(0, size[0] - 1, 0, size[1] - 1, 0, size[2] - 1)
+
+    if vtk.vtkVersion.GetVTKMajorVersion()<9:
+        print("Warning: VTK version <9.  No direction matrix.")
+    else:
+        vtk_image.SetDirectionMatrix(direction)
+
+    # depth_array = numpy_support.numpy_to_vtk(i2.ravel(), deep=True,
+    #                                          array_type = vtktype)
+    depth_array = numpy_support.numpy_to_vtk(i2.ravel())
+    depth_array.SetNumberOfComponents(ncomp)
+    vtk_image.GetPointData().SetScalars(depth_array)
+
+    vtk_image.Modified()
+    #
+    if debugOn:
+        print("Volume object inside sitk2vtk")
+        print(vtk_image)
+        #        print("type = ", vtktype)
+        print("num components = ", ncomp)
+        print(size)
+        print(origin)
+        print(spacing)
+        print(vtk_image.GetScalarComponentAsFloat(0, 0, 0, 0))
+
+    return vtk_image
